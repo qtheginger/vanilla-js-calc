0eb712dfc71552515125950318d17c4368124d57
Quinten Steenwyk <quinten.steenwyk@gmail.com>
2019-01-18 15:28:54 +0000

    added the functionality for number and operator keys

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
-keys.addEventListener('click', e =>{
-    if (e.target.matches('button')){
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
         const key = e.target;
         const action = key.dataset.action;
-        if (!action){
-            console.log('number key');
+        const keyContent = key.textContent;
+        const displayedNum  = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+        if (!action) {
+            if (displayedNum == '0' || previousKeyType == 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
         }
-        if (
-            action == 'add' ||
+        if (action == 'decimal') {
+            display.textContent = displayedNum + '.';   
+        }
+        if (action == 'add' ||
             action == 'subtract' ||
             action == 'multiply' ||
             action == 'divide'
-            ){
-                console.log('operator key');
-            }
-        else if (action == 'decimal'){
-            console.log('decimal');
+        ) {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
         }
-        else if (action == 'clear'){
-            console.log('clear');
-        }
-        else if (action == 'calculate'){
-            console.log('equal');
-        }
+        Array.from(key.parentNode.children)
+             .forEach(k => k.classList.remove('is-depressed'));
     }
 });
